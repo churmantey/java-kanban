f709604bf9005604df2f2da6fa2afe2d6a6c72b8
Danila Churmanteev <churmantey@yandex.ru>
2024-07-29 01:48:51 +0300

    Исправил обращение к полю времени на геттер.

--- src/dc/yandex/kanban/model/TaskStartTimeComparator.java
+++ src/dc/yandex/kanban/model/TaskStartTimeComparator.java
@@ -5,13 +5,13 @@ import java.util.Comparator;
 public class TaskStartTimeComparator implements Comparator<Task> {
     @Override
     public int compare(Task o1, Task o2) {
-        if (o1.startTime == null && o2.startTime == null) {
+        if (o1.getStartTime() == null && o2.getStartTime() == null) {
             return o1.getId() - o2.getId();
         }
-        if (o1.startTime == null) return 1;
-        if (o2.startTime == null) return -1;
-        if (o1.startTime.isBefore(o2.startTime)) return -1;
-        if (o1.startTime.isAfter(o2.startTime)) return 1;
+        if (o1.getStartTime() == null) return 1;
+        if (o2.getStartTime() == null) return -1;
+        if (o1.getStartTime().isBefore(o2.getStartTime())) return -1;
+        if (o1.getStartTime().isAfter(o2.getStartTime())) return 1;
         return 0;
     }
 }
